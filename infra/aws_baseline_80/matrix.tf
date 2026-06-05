dd18df4a16a8da02df4b7974fcc21742e7c11dbf
Gabriel Atkinson <g.atkinson112@gmail.com>
2026-06-04 18:19:40 -0700

    feat: implement iss-p14-002

---
+++ infra/aws_baseline_80/matrix.tf
@@ -0,0 +1,9 @@
+# ISS-P14-002: Core Matrix EC2 + encrypted EBS adoption
+# Ported/adapted per vault spec iss-p14-002
+# Locked: EC2 + encrypted EBS for v0, synapse.zenith-research.ca, federation 8448 enabled
+resource "aws_instance" "matrix" {
+  # stub for adoption; full impl in later tasks
+  ami           = var.matrix_ami
+  instance_type = var.matrix_instance_type
+  # TODO: EBS encryption, user_data from matrix_user_data.sh.tpl
+}
